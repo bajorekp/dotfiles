1288fc42f9cd5a202e2b9d6c18f7f6c39b7de488
Bajorek <bajorek.mail@gmail.com>
2021-12-20 17:55:47 +0100

    Install nerd font

--- setup/fonts.sh
+++ setup/fonts.sh
@@ -7,3 +7,8 @@ wget https://github.com/Twixes/SF-Mono-Powerline/archive/refs/heads/master.zip
 unzip master.zip
 cp -v SF-Mono-Powerline-master/SF-Mono-Powerline-*.otf ~/Library/Fonts
 rm -rf SF-Mono-Powerline-master master.zip
+
+wget https://github.com/epk/SF-Mono-Nerd-Font/archive/refs/heads/master.zip
+unzip master.zip
+cp -v SF-Mono-Nerd-Font-master/SFMono-*.otf ~/Library/Fonts
+rm -rf SF-Mono-Nerd-Font-master master.zip
