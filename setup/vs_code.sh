7dbfda9842bb3331ea376f91694a314accd1f962
bajorekp <bajorekp@gmail.com>
2022-01-01 13:20:18 +0100

    Update vs code config

--- setup/vs_code.sh
+++ setup/vs_code.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# Overwrite config
 ln -sf $HOME/Developer/dotfiles/vs_code/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
+
+# Create symlink to enable `code` command
+ln -s "/opt/homebrew/bin/code" "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
+
+# Restore extensions
+# To backup extensions issue: code --list-extensions > vscode_extensions_list.txt
+for extension_id in `cat vscode_extensions_list.txt`; do; code --install-extension $extension_id; done
