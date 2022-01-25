065a8446577d454a0a7dbb1c3017a52684cd2226
bajorekp <bajorekp@gmail.com>
2022-01-25 01:33:42 +0100

    Installs ruby 3.1

--- setup/ruby.sh
+++ setup/ruby.sh
@@ -9,3 +9,10 @@ gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D695
 # Installs RVM
 # backslash skips aliases
 \curl -sSL https://get.rvm.io | bash -s stable --ignore-dotfiles
+
+# Install required packages
+rvm requirements
+
+# Install Ruby 3
+# command = non interactive mode for rvm
+command rvm install 3.1.0
