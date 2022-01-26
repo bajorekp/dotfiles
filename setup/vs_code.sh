e746330267791f2b0654e2d71bcfab68bded169b
bajorekp <bajorekp@gmail.com>
2022-01-27 00:40:15 +0100

    Fix vs_code installation

--- setup/vs_code.sh
+++ setup/vs_code.sh
@@ -4,8 +4,11 @@
 ln -sf $HOME/Developer/dotfiles/vs_code/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
 
 # Create symlink to enable `code` command
-ln -s "/opt/homebrew/bin/code" "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
+ln -s "/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code" /opt/homebrew/bin/code
 
 # Restore extensions
+cd $HOME/Developer/dotfiles/vs_code
 # To backup extensions issue: code --list-extensions > vscode_extensions_list.txt
-for extension_id in `cat vscode_extensions_list.txt`; do; code --install-extension $extension_id; done
+for extension_id in `cat vscode_extensions_list.txt`; do
+  code --install-extension $extension_id
+done
