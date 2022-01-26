85ad2b91ad374112726bbb0c4ec7259e3e6b9f48
bajorekp <bajorekp@gmail.com>
2022-01-27 00:18:30 +0100

    RVM path to proceed with ruby installation

--- setup/ruby.sh
+++ setup/ruby.sh
@@ -9,6 +9,7 @@ gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D695
 # Installs RVM
 # backslash skips aliases
 \curl -sSL https://get.rvm.io | bash -s stable --ignore-dotfiles
+export PATH="$PATH:$HOME/.rvm/bin"
 
 # Install required packages
 rvm requirements
