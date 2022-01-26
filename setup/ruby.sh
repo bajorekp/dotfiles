f91329f8c5ac6393e8bd7dae12e3e8e0fb1434dc
bajorekp <bajorekp@gmail.com>
2022-01-27 00:39:57 +0100

    Don't install rvm when it is installed

--- setup/ruby.sh
+++ setup/ruby.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# Installs GPG if missing
-brew install gnupg
+if ! command -v rvm &> /dev/null; then
+  # Installs GPG if missing
+  brew install gnupg
 
-# Adds Michal Papis and Piotr Kuczynski signatures
-gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+  # Adds Michal Papis and Piotr Kuczynski signatures
+  gpg --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
-# Installs RVM
-# backslash skips aliases
-\curl -sSL https://get.rvm.io | bash -s stable --ignore-dotfiles
-export PATH="$PATH:$HOME/.rvm/bin"
+  # Installs RVM
+  # backslash skips aliases
+  \curl -sSL https://get.rvm.io | bash -s stable --ignore-dotfiles
+  export PATH="$PATH:$HOME/.rvm/bin"
 
-# Install required packages
-rvm requirements
+  # Install required packages
+  rvm requirements
 
-# Install Ruby 3
-# command = non interactive mode for rvm
-command rvm install 3.1.0
+  # Install a stable version of Ruby
+  # command = non interactive mode for rvm
+  command rvm install 3.1.0
+fi
