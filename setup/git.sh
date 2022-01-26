95f11ec0996b96f5c53cc3a66a94453c07e5822a
bajorekp <bajorekp@gmail.com>
2022-01-27 00:52:07 +0100

    Move out user configuration from .gitconfig

--- setup/git.sh
+++ setup/git.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 ln -sf $HOME/Developer/dotfiles/git/.gitconfig $HOME/.gitconfig
+cd $HOME/Developer/dotfiles/git
+cp user.gitconfig.example user.gitconfig
 
 # Setups GitHub and git auth
 gh auth login
