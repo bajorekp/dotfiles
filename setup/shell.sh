a3916f9a84cf78a4afa782e6635535950ebcf830
bajorekp <bajorekp@gmail.com>
2022-01-27 01:43:36 +0100

    Install oh-my-posh if missing

--- setup/shell.sh
+++ setup/shell.sh
@@ -4,4 +4,9 @@ mkdir -p ~/Library/local
 git clone git://github.com/RobSis/zsh-completion-generator.git ~/Library/local/zsh-completion-generator
 git clone git://github.com/zsh-users/zsh-completions.git ~/Library/local/zsh-completions
 
+if ! command -v oh-my-posh &> /dev/null; then
+  brew tap jandedobbeleer/oh-my-posh
+  brew install oh-my-posh
+fi
+
 ln -sf $HOME/Developer/dotfiles/zsh/.zshrc $HOME/.zshrc
