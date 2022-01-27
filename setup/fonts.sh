3e83f12895f458a06fe7be964e9dc3e53ed8494f
bajorekp <bajorekp@gmail.com>
2022-01-27 01:51:51 +0100

    Install fonts if missing

--- setup/fonts.sh
+++ setup/fonts.sh
@@ -3,12 +3,16 @@
 cd ~/Downloads
 
 # Installs powerline patched Mac font
-wget https://github.com/Twixes/SF-Mono-Powerline/archive/refs/heads/master.zip
-unzip master.zip
-cp -v SF-Mono-Powerline-master/SF-Mono-Powerline-*.otf ~/Library/Fonts
-rm -rf SF-Mono-Powerline-master master.zip
+if ! find ~/Library/Fonts -name 'SF-Mono-Powerline*.otf' | grep -q '.'; then
+  wget https://github.com/Twixes/SF-Mono-Powerline/archive/refs/heads/master.zip
+  unzip master.zip
+  cp -v SF-Mono-Powerline-master/SF-Mono-Powerline-*.otf ~/Library/Fonts
+  rm -rf SF-Mono-Powerline-master master.zip
+fi
 
-wget https://github.com/epk/SF-Mono-Nerd-Font/archive/refs/heads/master.zip
-unzip master.zip
-cp -v SF-Mono-Nerd-Font-master/SFMono-*.otf ~/Library/Fonts
-rm -rf SF-Mono-Nerd-Font-master master.zip
+if ! find ~/Library/Fonts -name 'SFMono*.otf' | grep -q '.'; then
+  wget https://github.com/epk/SF-Mono-Nerd-Font/archive/refs/heads/master.zip
+  unzip master.zip
+  cp -v SF-Mono-Nerd-Font-master/SFMono*.otf ~/Library/Fonts
+  rm -rf SF-Mono-Nerd-Font-master master.zip
+fi
