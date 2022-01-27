f4be6acf4015d004432dc502c629464f794505e1
bajorekp <bajorekp@gmail.com>
2022-01-27 01:17:46 +0100

    Turn on iTerm configuration

--- setup/terminal.sh
+++ setup/terminal.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 # Setup iTerm with configuration from ~/Developer/dotfiles/iterm directory
+defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
+defaults write com.googlecode.iterm2 NoSyncNeverRemindPrefsChangesLostForFile -bool true
+defaults write com.googlecode.iterm2 NoSyncNeverRemindPrefsChangesLostForFile_selection -int 2
 defaults write com.googlecode.iterm2 PrefsCustomFolder "$HOME/Developer/dotfiles/iterm"
